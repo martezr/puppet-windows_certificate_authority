03ea8a590f1f6c9bd56edc399c643ffa83ee5b17
Stephen Hoekstra <stephenhoekstra@gmail.com>
2014-01-29 00:15:26 +0000

    style and quoting fixes

--- manifests/enterprisesubca.pp
+++ manifests/enterprisesubca.pp
@@ -1,16 +1,11 @@
 # Configure an Enterprise Subordinate CA
-
 class windows_certificate_authority::enterprisesubca (
-  
-  $catype              = $windows_certificate_authority::catype, #
-  $parentca            = $windows_certificate_authority::parentca, #
-)
-{
-   # Deploy Server 2012 Active Directory Certificate Services Cerficate Authority
-   exec {'Deploy Enterprise Subordinate ADCS_CA':
-      command   => " Install-AdcsCertificationAuthority -Force -CAType $catype -ParentCA $parentca ",
-      provider  => powershell,
-      unless    => "((Get-CATemplate).Name).Contains('EFS')",
-       }
-       
-       }
+    $catype   = $windows_certificate_authority::catype,
+    $parentca = $windows_certificate_authority::parentca
+) {
+  exec { 'Deploy Enterprise Subordinate ADCS_CA':
+    command  => "Install-AdcsCertificationAuthority -Force -CAType $catype -ParentCA $parentca",
+    provider => powershell,
+    unless   => "((Get-CATemplate).Name).Contains('EFS')"
+  }
+}
